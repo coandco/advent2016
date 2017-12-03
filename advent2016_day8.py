60092fcf043d8d504cd72f6dcdb6576e5d824c1e
Clint Olson <clint.olson@zonarsystems.com>
2017-12-03 08:52:19 -0800

    Final solution for day 8.

--- advent2016_day8.py
+++ advent2016_day8.py
@@ -1,3 +1,6 @@
+import numpy
+import re
+
 INPUT = """rect 1x1
 rotate row y=0 by 5
 rect 1x1
@@ -143,3 +146,39 @@ rotate column x=11 by 5
 rotate column x=3 by 5
 rotate column x=2 by 5
 rotate column x=1 by 5"""
+
+
+def prettyprint_grid(grid):
+    total_lit = 0
+    for row in grid:
+        for item in row:
+            if item == 0:
+                print " ",
+            elif item == 1:
+                print "#",
+                total_lit += 1
+        print ""  # No trailing comma, so it should yield a newline
+    return total_lit
+
+GRID = numpy.zeros((6, 50))
+
+rect_regex = re.compile(r'^rect (\d+)x(\d+)')
+row_regex = re.compile(r'rotate row y=(\d+) by (\d+)')
+column_regex = re.compile(r'rotate column x=(\d+) by (\d+)')
+
+for line in INPUT.split("\n"):
+    match = rect_regex.match(line)
+    if match is not None:
+        GRID[:int(match.group(2)), :int(match.group(1))] = 1
+        continue
+    match = row_regex.match(line)
+    if match is not None:
+        GRID[int(match.group(1)), :] = numpy.roll(GRID[int(match.group(1)), :], int(match.group(2)))
+        continue
+    match = column_regex.match(line)
+    if match is not None:
+        GRID[:, int(match.group(1))] = numpy.roll(GRID[:, int(match.group(1))], int(match.group(2)))
+        continue
+
+num_lit = prettyprint_grid(GRID)
+print("Total lit pixels: %d" % num_lit)
