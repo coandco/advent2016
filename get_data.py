7d47885a31775d8cc468a03a20a5ee3a897898d9
Clint Olson <coandco@gmail.com>
2020-12-16 14:08:13 -0800

    Modify get_data to also create the python if it doesn't exist

--- get_data.py
+++ get_data.py
@@ -5,9 +5,13 @@ from pathlib import Path
 # for a given year/day and dump it into an automatically-named file.
 
 YEAR_NUM = 2016
-DAY_NUM = 8
+DAY_NUM = 10
 
 puzzle = Puzzle(year=YEAR_NUM, day=DAY_NUM)
 
 file_location = Path(f'inputs/advent{YEAR_NUM}_day{DAY_NUM:02d}_input.txt')
 file_location.write_text(puzzle.input_data)
+
+program_location = Path(f"advent{YEAR_NUM}_day{DAY_NUM:02d}.py")
+if not program_location.exists():
+    program_location.write_text("from utils import read_data")
